53a25ca93fb364c5599de1b95ed912f12184317d
mapleincode <llkj13579@126.com>
2022-06-08 12:03:47 +0800

    feat: lazy mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: maple
  * @Date: 2020-11-18 11:43:40
  * @LastEditors: maple
- * @LastEditTime: 2022-06-07 20:22:49
+ * @LastEditTime: 2022-06-08 11:56:56
  */
 const mysql = require('mysql2');
 mysql._createPool = mysql.createPool;
@@ -22,6 +22,108 @@ const quickConfig = require('./quick_config');
 
 const MODLE_MAP = {};
 const DB_MAP = {};
+const DB_CONFIG_MAP = {};
+
+function initDB (dbName, initOptions) {
+  if (!DB_CONFIG_MAP[dbName]) {
+    throw new Error(`db: ${dbName} not init!`);
+  }
+
+  const [dbType, dbConfig] = DB_CONFIG_MAP[dbName];
+  const db = DB_MAP[dbName] = new T.Toshihiko(dbType, dbConfig);
+
+  // add qdefine
+  db.qdefine = function (tableName, fields, options) {
+    const _fileds = quickConfig(fields, options);
+    const model = db.define(tableName, _fileds);
+
+    if (initOptions.saveTableWithNoDB) {
+      if (!MODLE_MAP[tableName]) {
+        // set model map
+        MODLE_MAP[tableName] = model;
+      } else {
+        // throw error;
+        throw new Error(`model ${tableName} has been already registed!`);
+      }
+    }
+
+    MODLE_MAP[`${name}.${tableName}`] = model;
+    return model;
+  };
+
+  // add
+  db.TYPE = T.Type;
+  return db;
+}
+
+function initModel (db, dbName, modelRoot, initOptions) {
+  const tableConfigs = [];
+  const dbDirPath = path.join(modelRoot, dbName);
+  const dirs = fs.readdirSync(dbDirPath);
+
+  // fetch table configs
+  for (const file of dirs) {
+    if (file[0] === '.') continue;
+    if (file.indexOf('.js') > -1) {
+      const config = require(path.join(dbDirPath, file));
+      tableConfigs.push(config);
+      continue;
+    }
+
+    const subRoot = path.join(dbDirPath, file);
+    const subDirs = fs.readdirSync(subRoot);
+    for (const file of subDirs) {
+      if (file[0] === '.') continue;
+      if (file.indexOf('.js') > -1) {
+        const config = require(path.join(subRoot, file));
+        tableConfigs.push(config);
+        continue;
+      }
+    }
+  }
+
+  // init model
+  for (const config of tableConfigs) {
+    if (!config.config) {
+      continue;
+    }
+
+    let [tableName, fields, options = {}] = config.config;
+
+    const field = fields[0];
+    if (typeof field !== 'object' || Array.isArray(field)) {
+      fields = quickConfig(fields, options);
+    }
+    const model = db.define(tableName, fields);
+    if (MODLE_MAP[tableName]) {
+      throw new Error(`table: ${tableName} already registed!`);
+    }
+
+    // set model map
+    if (initOptions.saveTableWithNoDB) {
+      if (!MODLE_MAP[tableName]) {
+        // set model map
+        MODLE_MAP[tableName] = model;
+      } else {
+        // throw error;
+        throw new Error(`model ${tableName} has been already registed!`);
+      }
+    }
+
+    MODLE_MAP[`${dbName}.${tableName}`] = model;
+
+    // bind function
+    const keys = Object.keys(config).filter(key => key !== 'config');
+    for (const key of keys) {
+      if (typeof config[key] === 'function') {
+        model[key] = config[key].bind(model);
+        continue;
+      }
+
+      model[key] = config[key];
+    }
+  }
+}
 
 module.exports = {
   db: {
@@ -37,30 +139,10 @@ module.exports = {
         delete dbConfig.dbType;
         dbConfig.database = dbConfig.database || name;
         // init toshihiko
-        const db = new T.Toshihiko(dbType, dbConfig);
+        // const db =
+        // DB_MAP[name] = db;
 
-        // add qdefine
-        db.qdefine = function (tableName, fields, options) {
-          const _fileds = quickConfig(fields, options);
-          const model = db.define(tableName, _fileds);
-
-          if (initOptions.saveTableWithNoDB) {
-            if (!MODLE_MAP[tableName]) {
-              // set model map
-              MODLE_MAP[tableName] = model;
-            } else {
-              // throw error;
-              throw new Error(`model ${tableName} has been already registed!`);
-            }
-          }
-
-          MODLE_MAP[`${name}.${tableName}`] = model;
-          return model;
-        };
-
-        // add
-        db.TYPE = T.Type;
-        DB_MAP[name] = db;
+        DB_CONFIG_MAP[name] = [dbType, dbConfig];
       }
 
       if (modelRoot) {
@@ -71,76 +153,12 @@ module.exports = {
           }
 
           const dbName = dbDir;
-          if (!DB_MAP[dbName]) {
-            throw new Error(`db: ${dbName} not init!`);
-          }
 
-          const db = DB_MAP[dbName];
-          const tableConfigs = [];
-          const dbDirPath = path.join(modelRoot, dbName);
-          const dirs = fs.readdirSync(dbDirPath);
-          for (const file of dirs) {
-            if (file[0] === '.') continue;
-            if (file.indexOf('.js') > -1) {
-              const config = require(path.join(dbDirPath, file));
-              tableConfigs.push(config);
-              continue;
-            }
+          // init db
+          const db = initDB(dbName, initOptions);
 
-            const subRoot = path.join(dbDirPath, file);
-            const subDirs = fs.readdirSync(subRoot);
-            for (const file of subDirs) {
-              if (file[0] === '.') continue;
-              if (file.indexOf('.js') > -1) {
-                const config = require(path.join(subRoot, file));
-                tableConfigs.push(config);
-                continue;
-              }
-            }
-          }
-
-          for (const config of tableConfigs) {
-            if (!config.config) {
-              continue;
-            }
-
-            let [tableName, fields, options = {}] = config.config;
-
-            const field = fields[0];
-            if (typeof field !== 'object' || Array.isArray(field)) {
-              fields = quickConfig(fields, options);
-            }
-            const model = db.define(tableName, fields);
-            if (MODLE_MAP[tableName]) {
-              throw new Error(`table: ${tableName} already registed!`);
-            }
-
-            // set model map
-            if (initOptions.saveTableWithNoDB) {
-              if (!MODLE_MAP[tableName]) {
-                // set model map
-                MODLE_MAP[tableName] = model;
-              } else {
-                // throw error;
-                throw new Error(`model ${tableName} has been already registed!`);
-              }
-            }
-
-            MODLE_MAP[`${dbName}.${tableName}`] = model;
-
-            // bind function
-            const keys = Object.keys(config).filter(key => key !== 'config');
-            for (const key of keys) {
-              if (typeof config[key] === 'function') {
-                model[key] = config[key].bind(model);
-                continue;
-              }
-
-              model[key] = config[key];
-            }
-
-            // return model;
-          }
+          // init model
+          initModel(db, dbName, modelRoot, initOptions);
         }
       }
     },
