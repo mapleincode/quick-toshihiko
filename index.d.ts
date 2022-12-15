bf9a0bacfe9ead286e52171cb8f93cb5e709a9d3
mapleincode <llkj13579@126.com>
2022-12-15 18:56:58 +0800

    chore: update version

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
  * @Author: maple
  * @Date: 2020-11-18 15:47:50
  * @LastEditors: maple
- * @LastEditTime: 2022-06-07 19:19:14
+ * @LastEditTime: 2022-12-15 18:55:51
  */
 import { Toshihiko, Model } from 'toshihiko'
 
@@ -10,12 +10,16 @@ import { Toshihiko, Model } from 'toshihiko'
 declare class ToshihikoQ extends Toshihiko {
   qdefine(collectionName: string, schema: any[], options?: any): Model;
 }
-declare {
-  db: {
-    function get(name: string): ToshihikoQ;
-    function init(dbConfigs: any[], modelRoot: string): void;
-  }
-  model: {
-    function get(tableName: string): Model;
-  }
+
+interface QDB {
+  get(name: string): ToshihikoQ;
+  init(dbConfigs: any[], modelRoot: string): void;
 }
+
+interface QModel {
+  get(tableName: string): Model;
+}
+
+declare const db: QDB;
+
+declare const model: QModel;
